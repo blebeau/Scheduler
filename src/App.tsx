d3700c8e7ec6edbe08928350444d05e9856e1e1b
Brock lebeau <brocklebeau338gmail.com>
2025-03-19 22:42:18 -0400

    Adding navigation bar, router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
 import "./App.css";
+import { BrowserRouter } from "react-router";
 import List from "./components/List";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <List name="test" role="test" currentLocation="test" nextLocations={[]} />
+      <div style={{ width: "20%" }}>
+        <Navbar />
+      </div>
+
+      <div style={{ width: "80%" }}>
+        {" "}
+        <BrowserRouter>
+          <List
+            name="test"
+            role="test"
+            currentLocation="test"
+            nextLocations={[]}
+          />
+        </BrowserRouter>
+      </div>
     </div>
   );
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,17 @@
+const Navbar = () => {
+  return (
+    <div
+      style={{
+        height: "100vh",
+        border: "1px black solid",
+        borderRadius: "5px",
+      }}
+    >
+      <h2>Home</h2>
+      <h2>Staff</h2>
+      <h2>Locations</h2>
+    </div>
+  );
+};
+
+export default Navbar;
