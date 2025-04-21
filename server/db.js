e18ccf8d8ff22fa4314424979ad6e7f7caa04ac3
Brock lebeau <brocklebeau338gmail.com>
2025-04-20 22:00:29 -0400

    Added post requests

--- server/db.js
+++ server/db.js
@@ -1,8 +1,5 @@
 const Pool = require("pg").Pool;
 
-
-console.log("env", process.env.port)
-
 const pool = new Pool({
 	user: process.env.USER,
 	password: process.env.PASSWORD,
--- server/index.js
+++ server/index.js
@@ -11,10 +11,10 @@ app.use(express.json());
 app.post("/book_schedule", async (req, res) => {
 	console.log('req.body', req.body)
 	try {
-		const { id, assigned_staff, location_id, start_time, end_time } = req.body;
+		const { schedule_id, assigned_staff, location_id, start_time, end_time } = req.body;
 
-		const newBooking = await pool.query("INSERT INTO schedule (id, assigned_staff, location_id, start_time, end_time) VALUES($1, $2, $3, $4, $5, $6)",
-			[id, assigned_staff, location_id, start_time, end_time]
+		const newBooking = await pool.query("INSERT INTO schedule (schedule_id, assigned_staff, location_id, start_time, end_time) VALUES($1, $2, $3, $4, $5)",
+			[schedule_id, assigned_staff, location_id, start_time, end_time]
 		)
 
 		res.json(newBooking);
@@ -22,6 +22,38 @@ app.post("/book_schedule", async (req, res) => {
 		console.log('err', err)
 	}
 })
+// add staff
+app.post("/add_staff", async (req, res) => {
+	console.log('req.body', req.body)
+	try {
+		const { id, first_name, last_name, staff_role, email, phone_number } = req.body;
+
+		const newStaff = await pool.query("INSERT INTO staff (id, first_name, last_name, staff_role, email, phone_number) VALUES($1, $2, $3, $4, $5, $6)",
+			[id, first_name, last_name, staff_role, email, phone_number]
+		)
+
+		res.json(newStaff);
+	} catch (err) {
+		console.log('err', err)
+	}
+})
+
+// Add location
+app.post("/add_location", async (req, res) => {
+	console.log('req.body', req.body)
+	try {
+		const { id, location_name } = req.body;
+
+		const newLocation = await pool.query("INSERT INTO locations (id, location_name) VALUES($1, $2)",
+			[id, location_name]
+		)
+
+		res.json(newLocation);
+	} catch (err) {
+		console.log('err', err)
+	}
+})
+
 
 // Get all staff
 
